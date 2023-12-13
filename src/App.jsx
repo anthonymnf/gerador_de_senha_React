f72f1887fccc8dbc8ccefcf55c7af1601bd63e79
anthonymnf <anthonymnf30@gmail.com>
2023-12-13 16:28:47 -0300

    Criação da estrutura nescessária e criação da função "copiar"

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [copyText, setCopyText] = useState("Copiar");
+  const [password, setpassword] = useState("Teste");
+  function generate() {
+    //Criar uma forma de criar uma senha aleatória
+    setCopyText("Copiar");
+  }
+  function copiar() {
+    window.navigator.clipboard.writeText(password);
+    setCopyText("Copiado");
+  }
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Gerador de senha</h1>
+        <button onClick={generate}>Gerar</button>
+        <button onClick={copiar}>{copyText}</button>
+        <div className="Divsenha">{password}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
