85cdbb3507db183da3a6c780afd56748e8bed01e
anthonymnf <anthonymnf30@gmail.com>
2023-12-14 09:26:42 -0300

    Estilização

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   const [copyText, setCopyText] = useState("Copiar");
-  const [password, setpassword] = useState("Teste");
+  const [password, setpassword] = useState("");
   function generate() {
     const characters =
       "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?";
@@ -21,11 +21,13 @@ function App() {
   }
   return (
     <>
-      <div>
+      <div className="container">
         <h1>Gerador de senha</h1>
         <button onClick={generate}>Gerar</button>
-        <button onClick={copiar}>{copyText}</button>
-        <div className="Divsenha">{password}</div>
+        <button className="btn2" onClick={copiar}>
+          {copyText}
+        </button>
+        <div>{password}</div>
       </div>
     </>
   );
