96dec28b7c0b7c343320e23802fcbe7742537211
anthonymnf <anthonymnf30@gmail.com>
2023-12-15 15:52:05 -0300

    Add Input State

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import "./App.css";
 function App() {
   const [copyText, setCopyText] = useState("Copiar");
   const [password, setpassword] = useState("");
+  const [passwordSize, setpasswordSize] = useState("12");
   function generate() {
     const characters =
       "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?";
     let newsenha = "";
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < passwordSize; i++) {
       const position = Math.floor(Math.random() * characters.length);
       newsenha += characters[position];
     }
@@ -23,11 +24,21 @@ function App() {
     <>
       <div className="container">
         <h1>Gerador de senha</h1>
+        <div>
+          <label htmlFor="passwordSize">Tamanho: </label>
+          <input
+            type="number"
+            id="passwordSize"
+            min={1}
+            value={passwordSize}
+            onChange={(ev) => setpasswordSize(ev.target.value)}
+          />
+        </div>
         <button onClick={generate}>Gerar</button>
         <button className="btn2" onClick={copiar}>
           {copyText}
         </button>
-        <div>{password}</div>
+        <div>Sua senha segura: {password}</div>
       </div>
     </>
   );
